34691698b039b6e0199d8ac06b6f8141296d9333
Michał Kalbarczyk <fazibear@gmail.com>
2018-08-18 18:12:31 +0200

    Add application file

---
+++ app/application.rb
@@ -0,0 +1,34 @@
+# require Inesita
+require 'inesita'
+require 'inesita-router'
+
+require 'sid'
+require 'web-midi'
+
+require 'bowser/window'
+require 'bowser/http'
+
+require 'router'
+require 'asid'
+require 'store_list'
+require 'store_tree'
+require 'store'
+
+require_tree './components'
+
+class Application
+  include Inesita::Component
+
+  inject Store
+  inject Router
+
+  def render
+    div id: 'screen' do
+      component router
+    end
+  end
+end
+
+Inesita::Browser.ready? do
+  Application.mount_to(Inesita::Browser.body)
+end
